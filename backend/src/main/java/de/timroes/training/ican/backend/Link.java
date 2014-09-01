e58d6d2aa4c362adde0cb64a111eb21f2040f432
Daniel Bälz <daniel.baelz@lysandor.de>
2014-09-01 22:39:36 +0200

    Comments for the app engine backend

--- backend/src/main/java/de/timroes/training/ican/backend/Link.java
+++ backend/src/main/java/de/timroes/training/ican/backend/Link.java
@@ -4,11 +4,14 @@ import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
 /**
+ * The entity, which contains the necessary data to represent a link.
  * @author Tim Roes <mail@timroes.de>
  */
+// Annotation for Objectify to label this class as an entity.
 @Entity
 public class Link {
 
+    // Each entity requires an identifier, which can be either a string or an integer.
 	@Id
 	private String id;
 
@@ -16,13 +19,22 @@ public class Link {
 	private String description;
 	private String author;
 
+    // There must be a no-arg constructor for Objectify to persist the data.
 	public Link() { }
 
+    /**
+     * Constructor to create a new link object. Prepends "http://" to the url if the url doesn't
+     * start with http or https protocol.
+     * @param id the generated id of the entity
+     * @param author the author name
+     * @param description the description for the url
+     * @param url the url
+     */
 	public Link(String id, String author, String description, String url) {
 		this.id = id;
 		this.author = author;
 		this.description = description;
-		// If the url doesn't start with http:// or https:// append an http:// in front of it.
+		// If the url doesn't start with http:// or https:// prepend "http://".
 		if(!url.startsWith("http://") && !url.startsWith("https://")) {
 			this.url = "http://" + url;
 		} else {
@@ -30,14 +42,26 @@ public class Link {
 		}
 	}
 
+    /**
+     * Returns the url of the link. This method is necessary to serialize the field.
+     * @return the url
+     */
 	public String getUrl() {
 		return url;
 	}
 
+    /**
+     * Returns the description of the link. This method is necessary to serialize the field.
+     * @return the description
+     */
 	public String getDescription() {
 		return description;
 	}
 
+    /**
+     * Returns the author of the link. This method is necessary to serialize the field.
+     * @return the author
+     */
 	public String getAuthor() {
 		return author;
 	}
--- backend/src/main/java/de/timroes/training/ican/backend/MyEndpoint.java
+++ backend/src/main/java/de/timroes/training/ican/backend/MyEndpoint.java
@@ -16,14 +16,24 @@ import com.googlecode.objectify.ObjectifyService;
 import java.util.List;
 import java.util.UUID;
 
-
-/** An endpoint class we are exposing */
+/**
+ * The endpoint class with the descriptions of the REST interface.
+ */
+// Description of the REST interface, starting with the "@Api" annotation.
+// name: the name of the REST interface.
+// version: the version of the endpoint/REST interface, which should be changed when changing the API.
+// namespace: causes the generated client libraries to have the specified namespace/package name.
 @Api(name = "ican", version = "v1", namespace = @ApiNamespace(ownerDomain = "backend.ican.training.timroes.de", ownerName = "backend.ican.training.timroes.de", packagePath=""))
 public class MyEndpoint {
 
 	private Objectify objectify;
 
+    /**
+     * Helper method to ensure that the entity is registered before using Objectify.
+     * @return the objectify object
+     */
 	private Objectify ofy() {
+        // If the objectify variable hasn't been initialized, register the link entity.
 		if(objectify == null) {
 			ObjectifyService.register(Link.class);
 			objectify = ObjectifyService.ofy();
@@ -31,15 +41,38 @@ public class MyEndpoint {
 		return objectify;
 	}
 
+    /**
+     * The method that adds a new link and persist the data with Objectify.
+     * @param description the description for the url
+     * @param url the url
+     * @param author the author name
+     * @return the created link object with the given arguments
+     */
+    // The @ApiMethod annotation allows an API configuration.
+    // name: the name for this method in the generated client library and API.
+    // @Named: this annotation is required for all non-entity type parameters passed to backend methods.
 	@ApiMethod(name = "addLink")
 	public Link addNewLink(@Named("description") String description, @Named("url") String url, @Named("author") String author) {
+        // Creates a link object with a random UUID
 		Link l = new Link(UUID.randomUUID().toString(), author, description, url);
+
+        // The save() method starts a save command chain, which allows you to save multiple entity objects.
+        // The entity(l) method saves a single entity in the datastore and retrieves the result with the now() method.
 		ofy().save().entity(l).now();
 		return l;
 	}
 
+    /**
+     * The method that loads and returns all link entries.
+     * @return a list of all link entries
+     */
+    // The @ApiMethod annotation allows an API configuration.
+    // name: the name for this method in the generated client library and API.
 	@ApiMethod(name = "links")
 	public List<Link> getLinks() {
+        // The load() method starts a load command chain to fetch data from the datastore.
+        // The type(Link.class) method restricts the find operation to entities of the link entity.
+        // It returns the results as a java.util.List with the list() method.
 		return ofy().load().type(Link.class).list();
 	}
 
